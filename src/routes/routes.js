8a4d28bd4f62d097c72c6a3cc36aa5bafaa8be0a
Felipe Pires <97983021+nsx07@users.noreply.github.com>
2023-11-16 11:27:05 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,59 +1,7 @@
-import { vapidKeysObject } from "../environment/environment.js"
 import { EmailService } from "../services/email-service.js";
 import { Router } from "express";
-import webpush from "web-push";
-
-webpush.setVapidDetails(
-    vapidKeysObject.subject,
-    vapidKeysObject.publicKey,
-    vapidKeysObject.privateKey
-);
 
 export const router = Router();
-
-router.post("/notify", async (req, res) => {
-    console.log(req.body);
-    
-    let allSubscriptions = req.body.subscriptions;
-
-    if (allSubscriptions) {
-        allSubscriptions = Array.from(allSubscriptions).map(x => JSON.parse(x.subscriptionObject));
-    }
-
-    const notificationPayload = req.body.payload ?? {
-        "notification": {
-            "title": "Base test",
-            "body": "Testing Push Notifications!",
-            "vibrate": [100, 50, 100],
-        }
-    };
-
-    if (!allSubscriptions) {
-        res.status(404)
-        res.send({message: "No subscriber found."})
-        req.next();
-        return undefined;
-    } else if (allSubscriptions.length < 1) {
-        res.status(404)
-        res.send({message: "No subscriber found."})
-        req.next();
-        return undefined;
-    }
-
-    console.log(allSubscriptions);
-
-    Promise.all(allSubscriptions.map(sub => webpush.sendNotification(
-        sub, JSON.stringify(notificationPayload) )))
-        .then((x) => res.status(200).json({message: 'Notification sent successfully.', record: x}))
-        .catch(err => {
-            console.error("Error sending notification, reason: ", err);
-            res.status(500).send({error: err});
-        });
-});
-
-router.get("/publicKey", (req, res) => {
-    res.status(200).send({publicKey: vapidKeysObject.publicKey});
-});
 
 router.post("/email/send", async (req, res) => {
     let body = req.body;
