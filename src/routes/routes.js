dad820f80c3ea4ee26c043d3066a39343f96a010
Felipe Pires <97983021+nsx07@users.noreply.github.com>
2023-11-16 11:26:19 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -55,25 +55,37 @@ router.get("/publicKey", (req, res) => {
     res.status(200).send({publicKey: vapidKeysObject.publicKey});
 });
 
-router.post("/email", (req, res) => {
-    const transporter = EmailService.getDefaultTransporter();
-        //     req.body.transporter 
-        // ? EmailService.customTransporter(req.body.transporter)
-        // : EmailService.getDefaultTransporter();
+router.post("/email/send", async (req, res) => {
+    let body = req.body;
 
-    console.log(req.body);
+    if (body.transporter) {
+        let transporter = emailService.customTransporter(body.transporter);
+        let error = null;
 
-    if (req.body) {
-        transporter.sendMail(req.body, function(err, data) {
-            if (err) {
-              console.log("Error " + err);
-              res.status(503).send({message: "Error " + err});
+        if (body.mail) {
+            
+            let promise = await new Promise((res, rej) => {
+                transporter.sendMail(body.mail, function(err, data) {
+                    if (err) {
+                      console.log(err);
+                      error = err;
+                      res(err)
+                    } else {
+                        console.log("Email sent successfully");
+                        res(data)
+                    }
+                })
+            })
+
+            if (error) {
+                return res.status(503).send(JSON.stringify(error))
             } else {
-              console.log("Email sent successfully");
-              res.status(200).send({message: "Email sent successfully"});
+                res.status(200).send({message: "Email sent successfully"});
             }
-          });
-    } else {
-        res.status(400).send({message: "Email body is missing and its required."})
+        } else {
+            return res.status(400).send({message: "Email body is missing and its required."})
+        }
     }
+    
+    return res.status(400).send({message: "Transporter not found. Its mandatory!"})
 });
